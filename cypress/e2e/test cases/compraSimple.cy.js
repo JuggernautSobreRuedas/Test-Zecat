c29c4d77fe96277bc0d2e2183e5ceb929a784703
JuggernautSobreRuedas <118290085+JuggernautSobreRuedas@users.noreply.github.com>
2022-11-18 07:55:43 -0300

    Update compraSimple.cy.js

--- cypress/e2e/test cases/compraSimple.cy.js
+++ cypress/e2e/test cases/compraSimple.cy.js
@@ -1,11 +1,25 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+})
 describe("Set de pruebas / Muestra Simple", ()=>{
     beforeEach(()=>{
         cy.viewport(1280,800)
     })
     it("Verificar cantidad de un producto", function(){
+        //login
         cy.visit("http://zecat-backoffice-stage.s3-website-us-east-1.amazonaws.com/login", {failOnStatusCode: false});
         cy.get("[name=username]").type("vbustamante@mobeats.io", {delay : 50});
         cy.get("[name=password]").type("12345678", {delay : 50});
-        cy.get("[type=submit").click()
+        cy.get("[type=submit]").click()
+        cy.get(':nth-child(2) > .menu-link').click()
+        cy.get('.m-right-1.el-input > .el-input__inner').type("Gorro Acrylic")
+        cy.get('.m-bottom-4 > :nth-child(2)').click()
+        cy.wait(18000)
+        cy.get('.cell').contains("Gorro Acrylic")
+        cy.get('.detail-link row center middle')
+        
+
     })
 })
