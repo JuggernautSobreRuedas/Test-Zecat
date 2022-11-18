97e8161faadabb64e41459c6f8b76f97215f3f34
JuggernautSobreRuedas <118290085+JuggernautSobreRuedas@users.noreply.github.com>
2022-11-18 13:20:53 -0300

    Update compraSimple.cy.js

--- cypress/e2e/test cases/compraSimple.cy.js
+++ cypress/e2e/test cases/compraSimple.cy.js
@@ -4,22 +4,78 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     return false
 })
 describe("Set de pruebas / Muestra Simple", ()=>{
+    var priceBack
+    var priceEcommerce$
+    var product1 = "Gorro Acrylic"
+    var product2 = "Bol Touch Screen"
+    var product3 = "Remera Quina"
     beforeEach(()=>{
         cy.viewport(1280,800)
     })
-    it("Verificar cantidad de un producto", function(){
-        //login
-        cy.visit("http://zecat-backoffice-stage.s3-website-us-east-1.amazonaws.com/login", {failOnStatusCode: false});
-        cy.get("[name=username]").type("vbustamante@mobeats.io", {delay : 50});
-        cy.get("[name=password]").type("12345678", {delay : 50});
-        cy.get("[type=submit]").click()
-        cy.get(':nth-child(2) > .menu-link').click()
-        cy.get('.m-right-1.el-input > .el-input__inner').type("Gorro Acrylic")
-        cy.get('.m-bottom-4 > :nth-child(2)').click()
-        cy.wait(18000)
-        cy.get('.cell').contains("Gorro Acrylic")
-        cy.get('.detail-link row center middle')
-        
-
+    //Producto1
+    it("Tomar verificar del producto en BO", function(){
+        cy.request("https://d2rb7slziwouc6.cloudfront.net/generic_product/3750")
+        .its("body")
+        .its("generic_product")
+        .its("price")
+        .then($price =>{
+            priceBack = $price
+            cy.log(priceBack)
+        })
+    })
+    it("Validar que el precio en el ecommerce sea el correcto",()=>{
+        cy.visit("http://stage-front.wu6hmgnifa.us-east-1.elasticbeanstalk.com")
+        cy.get(".search-input").type(product1, {delay:200})
+        cy.get(".search-input").type('{enter}')
+        cy.get('.hide-mobile > .product-name').then($priceEcommerce =>{
+            priceEcommerce$ = $priceEcommerce.text()
+            cy.log(priceEcommerce$)
+            expect(priceEcommerce$.slice(1).replace(".", "")).to.be.eq(String(priceBack).replace(".", "")) 
+            
+        })
+    })
+    //Producto2
+    it("Tomar verificar del producto en BO", function(){
+        cy.request("https://d2rb7slziwouc6.cloudfront.net/generic_product/3581")
+        .its("body")
+        .its("generic_product")
+        .its("price")
+        .then($price =>{
+            priceBack = $price
+            cy.log(priceBack)
+        })
+    })
+    it("Validar que el precio en el ecommerce sea el correcto",()=>{
+        cy.visit("http://stage-front.wu6hmgnifa.us-east-1.elasticbeanstalk.com")
+        cy.get(".search-input").type(product2, {delay:200})
+        cy.get(".search-input").type('{enter}')
+        cy.get('.hide-mobile > .product-name').first().then($priceEcommerce =>{
+            priceEcommerce$ = $priceEcommerce.text()
+            cy.log(priceEcommerce$)
+            expect(priceEcommerce$.slice(1).replace(".", "")).to.be.eq(String(priceBack).replace(".", "")) 
+            
+        })
+    })
+    //Producto3
+    it("Tomar verificar del producto en BO", function(){
+        cy.request("https://d2rb7slziwouc6.cloudfront.net/generic_product/4762")
+        .its("body")
+        .its("generic_product")
+        .its("price")
+        .then($price =>{
+            priceBack = $price
+            cy.log(priceBack)
+        })
+    })
+    it("Validar que el precio en el ecommerce sea el correcto",()=>{
+        cy.visit("http://stage-front.wu6hmgnifa.us-east-1.elasticbeanstalk.com")
+        cy.get(".search-input").type(product3, {delay:200})
+        cy.get(".search-input").type('{enter}')
+        cy.get('.hide-mobile > .product-name').then($priceEcommerce =>{
+            priceEcommerce$ = $priceEcommerce.text()
+            cy.log(priceEcommerce$)
+            expect(priceEcommerce$.slice(1).replace(".", "")).to.be.eq(String(priceBack).replace(".", "")) 
+            
+        })
     })
 })
